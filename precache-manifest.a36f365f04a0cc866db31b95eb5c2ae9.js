d2c29c105077b7687d845a0e0afab1bf40a4e669
Andrei Solomonov <justbenya@gmail.com>
2020-09-14 17:13:40 +0700

    Updates

---
+++ precache-manifest.a36f365f04a0cc866db31b95eb5c2ae9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7ae0bd56f3b1db2b1355855376625fb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "48c4a6a335e52fa26aa7",
+    "url": "./static/css/main.bcae5eea.chunk.css"
+  },
+  {
+    "revision": "a91cb6c49d59bb39b185",
+    "url": "./static/js/2.5fe3bfcb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.5fe3bfcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48c4a6a335e52fa26aa7",
+    "url": "./static/js/main.fbe4956c.chunk.js"
+  },
+  {
+    "revision": "7a5935fb235ed4399616",
+    "url": "./static/js/runtime-main.4de2ceb3.js"
+  }
+]);
